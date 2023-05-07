e0f5365799c4a961f81057c18698e4601f595cef
Safia Ali <ali.swaphiyah@gmail.com>
2023-05-07 09:45:01 -0500

    updated userid in createthought

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -16,7 +16,7 @@ module.exports = {
 	createThought(req, res) {
 		Thought.create(req.body)
 			.then((thought) => {
-				return User.findOneAndUpdate({ _id: req.body._id }, { $addToSet: { thoughts: thought._id} }, { new: true });
+				return User.findOneAndUpdate({ _id: req.body.userId  }, { $addToSet: { thoughts: thought._id} }, { new: true });
 			})
 			.then((user) => (!user ? res.status(404).json({ message: "Post created, but found no user with that ID" }) : res.json("Created the post 🎉")))
 			.catch((err) => {
